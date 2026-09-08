ff211039c853d450ed5442deeaa21dccf107c92a
JOAO VITOR GUIARO GOMES <joao.gomes8@portalsesisp.org.br>
2026-09-08 15:48:36 -0300

    Marker in map

---
+++ lib/main_Mapa.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+void main(){
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget{
+  const MeuApp({super.key});
+
+  @override 
+  Widget build(BuildContext context){
+    return MaterialApp( // Adicionado const aqui para boas práticas
+      debugShowCheckedModeBanner: false,
+      title: 'Meu Mapa',
+      home: const MapaPage(),
+    );
+  }
+}
+
+class MapaPage extends StatelessWidget{
+  const MapaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Meu Mapa')),
+
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(-21.470000, -47.030000),
+          initialZoom: 13,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}{x}{y}.png3',
+            userAgentPackageName: 'com.exemplo.mapa_flutter',
+          )
+        ],
+      ),
+    );
+  }
+}
